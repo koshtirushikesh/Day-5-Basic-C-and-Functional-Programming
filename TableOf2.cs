53873c7a84aaa064e494453e769a57f37eef1ded
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-16 11:39:28 +0530

    Power of 2

---
+++ TableOf2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logical_programing
+{
+    public class TableOf2
+    {
+        public static void Table_of_2()
+        {
+            Console.WriteLine("Enter The Power Value ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int Table = 1;
+            Console.WriteLine(Table);
+
+            for (int i = 0; i < N; i++)
+            {
+                Table = Table * 2;
+                Console.WriteLine(Table);
+            }
+
+        }
+    }
+}
