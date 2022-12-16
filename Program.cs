709b295921e5a95c41791945229568d834e59045
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-16 11:28:21 +0530

    Program file

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicCorePrograming
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Swap.SwapNum();
+            QuotientAndRemainder.FindQutientAndRemainder();
+        }
+    }
+}
